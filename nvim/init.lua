db2ba6f7d1136dc512c83d9a1b0546d2b5d6540d
André Yermak Naumenko <asyermak@gmail.com>
2026-04-13 18:19:05 +0200

    Missing lines init

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,17 @@
-  if not (vim.uv or vim.loop).fs_stat(pckr_path) then
-    vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/lewis6991/pckr.nvim",
-      pckr_path,
-    })
-  end
+local function bootstrap_pckr()
+	local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
 
-  vim.opt.rtp:prepend(pckr_path)
+	if not (vim.uv or vim.loop).fs_stat(pckr_path) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/lewis6991/pckr.nvim",
+			pckr_path,
+			})
+	end
+
+	vim.opt.rtp:prepend(pckr_path)
 end
 
 bootstrap_pckr()
