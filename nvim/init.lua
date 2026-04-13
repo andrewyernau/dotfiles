812eda871456d96def5c2ecdbb4417ee2d5ea5a4
André Yermak Naumenko <asyermak@gmail.com>
2026-04-13 18:16:09 +0200

    Improve bootstrap nvim init

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,17 @@
-vim.cmd("packadd pckr.nvim")
+  if not (vim.uv or vim.loop).fs_stat(pckr_path) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/lewis6991/pckr.nvim",
+      pckr_path,
+    })
+  end
+
+  vim.opt.rtp:prepend(pckr_path)
+end
+
+bootstrap_pckr()
 
 require("config.options")
 require("config.keymaps")
